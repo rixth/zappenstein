c44e0a261a2fa9e3eaa7c1cc522e7fdfd3ee026f
Tom Rix <tom@rixth.org>
2011-05-14 11:23:04 -0700

    Public directories and basic layout/view thingo

--- app/server.rb
+++ app/server.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 
 module ZapposChallenge
   class Server < Sinatra::Base
+    set :public, File.dirname(__FILE__) + '/../public'
+    
     get '/' do
       haml :index
     end
