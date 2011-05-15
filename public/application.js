186d5ce114547f42bc653c9d0f2b3cfbea7c6969
Tom Rix <tom@rixth.org>
2011-05-15 13:56:57 -0700

    Get a generic data source going

--- public/application.js
+++ public/application.js
@@ -1,6 +1,18 @@
 // Global. Unawesome.
 var API_KEY = '1036d75d74f2ed2b0b5a3beabf0e36bec8149ae2';
 
+(function ($) {
+  $.fn.setSlideContent = function (content) {
+    return $(this).each(function () {
+      var slide = $(this);
+      slide.addClass('contentChange');      
+      setTimeout(function () {
+        slide.removeClass('contentChange').find('.content').html(content);
+      }, 600);
+    });
+  }
+}(jQuery));
+
 var SlideManager = new (function () {
   var columnMapping = ['purchase', 'randomProduct', 'tweet', 'randomProduct', 'purchase'],
       slides = {};
@@ -19,62 +31,71 @@ var SlideManager = new (function () {
   };
 });
 
-(function ($) {
-  $.fn.setSlideContent = function (content) {
-    return $(this).each(function () {
-      var slide = $(this);
-      slide.addClass('contentChange');      
-      setTimeout(function () {
-        slide.removeClass('contentChange').find('.content').html(content);
-      }, 600);
-    });
+var DataSource = function (options) {
+  var items = [],
+      loadCallbacks = [];
+  
+  this.loadData = function (callback) {
+    if (!loadCallbacks.length) {
+      $.ajax(typeof(options.url) === 'function' ? options.url() : options.url, {
+        dataType: 'jsonp'
+      }).success(function (data) {
+        items = data[options.dataKey || 'results'];
+        loadCallbacks.forEach(function (callback) {
+          callback();
+        })
+        loadCallbacks = [];
+      });
+    }
+    loadCallbacks.push(callback);
   }
-}(jQuery));
+  
+  this.getItem = function (callback) {
+    if (!items.length) {
+      return this.loadData(function () {
+        callWithItem(callback);
+      });
+    }
+    callWithItem(callback);
+  }
+  
+  function callWithItem(callback) {
+    var item = items.pop();
+    if (options.itemCanBeDisplayed) {
+      while (item && !options.itemCanBeDisplayed(item)) {
+        item = items.pop();
+      }
+    }
+    
+    if (item) {
+      callback(item);
+    }
+  }
+};
+
+DataSource.purchases = (function () {
+  var displayedZips = [];
+  return new DataSource({
+    url: 'http://api.zappos.com/Statistics?type=latestStyles&key=' + API_KEY,
+    itemCanBeDisplayed: function (item) {
+      var displayed = displayedZips.indexOf(item.zip) === -1;
+      displayedZips.push(item.zip);
+      return displayed;
+    }
+  });
+  
+  setInterval(function () {
+    displayedZips = [];
+  }, 300000);
+}());
 
 /**
  * This is the code that 
  */
 $(function () {
-  var renderEvery = 2000,
-      items = [],
-      zipsTracked = [];
-  
-  function fetchData() {
-    $.ajax('http://api.zappos.com/Statistics?type=latestStyles&key=' + API_KEY, {
-      dataType: 'jsonp'
-    }).success(function (data) {
-      items = data.results;
-      render(items.pop());
-    });
-  }
-  
-  function render(item) {
-    SlideManager.getSlide('purchase').setSlideContent('<img src="http://maps.google.com/maps/api/staticmap?center=' + item.zip + ',' + item.state + '&zoom=12&size=263x200&maptype=roadmap&sensor=false"/>');
-
-    if (items.length) {
-      setTimeout(function() {
-        var item = items.pop();
-        while (items.length && zipsTracked.indexOf(item.zip) !== -1) {
-          item = items.pop();
-        }
-        
-        if (!items.length && !item) {
-          // If we're out of items, go fetch some more
-          return fetchData();
-        }
-        
-        zipsTracked.push(item.zip);
-        render(item);
-      }, renderEvery);
-    } else {
-      
-    }
-  }
-  
-  fetchData();
-  
-  // Clear the zips displayed every 5 minutes
   setInterval(function () {
-    zipsTracked = [];
-  }, 300000);
+    DataSource.purchases.getItem(function (item) {
+      SlideManager.getSlide('purchase').setSlideContent('<img src="http://maps.google.com/maps/api/staticmap?center=' + item.zip + ',' + item.state + '&zoom=12&size=263x200&maptype=roadmap&sensor=false"/>');
+    });
+  }, 2000)
 });
