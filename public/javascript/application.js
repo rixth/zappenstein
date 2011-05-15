93556ec2b2712a3e43946ce5264213b26336352d
Tom Rix <tom@rixth.org>
2011-05-15 14:14:14 -0700

    Clear up the recent purchases API

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,5 +1,5 @@
 // Global. Unawesome.
-var API_KEY = '1036d75d74f2ed2b0b5a3beabf0e36bec8149ae2';
+var ZAPPOS_API_KEY = '1036d75d74f2ed2b0b5a3beabf0e36bec8149ae2';
 
 (function ($) {
   $.fn.setSlideContent = function (content) {
@@ -16,7 +16,7 @@ var API_KEY = '1036d75d74f2ed2b0b5a3beabf0e36bec8149ae2';
 DataSource.purchases = (function () {
   var displayedZips = [];
   return new DataSource({
-    url: 'http://api.zappos.com/Statistics?type=latestStyles&key=' + API_KEY,
+    url: 'http://api.zappos.com/Statistics?type=latestStyles&key=' + ZAPPOS_API_KEY,
     itemCanBeDisplayed: function (item) {
       var displayed = displayedZips.indexOf(item.zip) === -1;
       displayedZips.push(item.zip);
@@ -30,12 +30,18 @@ DataSource.purchases = (function () {
 }());
 
 /**
- * This is the code that 
+ * This is the renderer for recent purchases.
  */
 $(function () {
   setInterval(function () {
     DataSource.purchases.getItem(function (item) {
-      SlideManager.getSlide('purchase').setSlideContent('<img src="http://maps.google.com/maps/api/staticmap?center=' + item.zip + ',' + item.state + '&zoom=12&size=263x200&maptype=roadmap&sensor=false"/>');
+      SlideManager.getSlide('purchase').setSlideContent([
+        staticMapTag(item)
+      ].join(''));
     });
-  }, 2000)
+  }, 2000);
+  
+  function staticMapTag(item) {
+    return '<img src="http://maps.google.com/maps/api/staticmap?center=' + item.zip + ',' + item.state + '&zoom=12&size=263x200&maptype=roadmap&sensor=false"/>';
+  }
 });
